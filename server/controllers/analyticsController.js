cfb949ee9faf8f89d67d3beddbed96410c35634e
kevinnammour <nammourkevin@gmail.com>
2022-05-07 08:52:09 +0300

    branches income

--- server/controllers/analyticsController.js
+++ server/controllers/analyticsController.js
@@ -1,4 +1,5 @@
 const Invoice = require("../models/Invoice");
+const Branch = require("../models/Branch");
 
 const getTotalIncome = async (req, res) => {
   var starter = new Date();
@@ -35,9 +36,57 @@ const getTotalIncome = async (req, res) => {
     });
 };
 
-const getBranchesIncome = async (req, res) => {};
+const getBranchesIncome = async (req, res) => {
+  var ts = new Date().getTime();
+  let date = new Date(ts).toLocaleString("en-US", {
+    timeZone: "Asia/Beirut",
+  });
+  currDay = date.substring(0, date.indexOf(","));
+  var today = new Date(currDay);
 
-const getBranchIncome = async (req, res) => {};
+  let data = [];
+  let branchIds = [];
+
+  Branch.find({}, "name")
+    .then((branches) => {
+      for (let branch of branches) {
+        branchIds.push(branch._id);
+        data.push([branch.name, 0]);
+      }
+      Invoice.find({}, "branchId amount datetime").then(async (invoices) => {
+        const promise = invoices.map((invoice) => {
+          var formattedDatetime = invoice.datetime.substring(
+            0,
+            invoice.datetime.indexOf(",")
+          );
+          if (
+            Math.floor(
+              Math.abs(today - new Date(formattedDatetime)) /
+                (1000 * 60 * 60 * 24) <=
+                30
+            )
+          ) {
+            for (let i = 0; i < branchIds.length; i++) {
+              if (branchIds[i].toString() == invoice.branchId.toString()) {
+                data[i][1] += invoice.amount;
+              }
+            }
+          }
+        });
+        await Promise.all(promise);
+        return res.status(200).json(data);
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({ message: err.message });
+    });
+};
+
+const getBranchIncome = async (req, res) => {
+  if (!req?.params?.branchId) {
+    return res.status(400).json({ message: "Branch id required." });
+  }
+};
 
 module.exports = {
   getTotalIncome,
